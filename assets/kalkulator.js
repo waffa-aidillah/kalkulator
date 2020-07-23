1b886c184a938ed2050ae03eb27008cf9aa688f1
Waffa Aidillah <waffaaidillah23@gmail.com>
2020-07-23 23:26:58 +0700

    mengoperasikan kalkulator

---
+++ assets/kalkulator.js
@@ -0,0 +1,95 @@
+//create calculator object
+const calculator= {
+    displayNumber: '0',
+    operator: null,
+    firtNumber:null,
+    waitingForSecondNumber:false
+};
+//menampilkan angka di display
+function updateDisplay(){
+    document.querySelector('#displayNumber').innerText = calculator.displayNumber;
+}
+//fungsi clear
+function clearCalculator(){
+    calculator.displayNumber= '0';
+    calculator.operator=null;
+    calculator.firtNumber=null;
+    calculator.waitingForSecondNumber=false;
+}
+//fungsi positif negatif
+function inverseNumber(){
+    if(calculator.displayNumber==='0'){
+        return; //langsung keluar function
+    }
+    calculator.displayNumber *= -1;
+    
+}
+//fungsi operasi matematika
+function handleOperator(operator){
+    if(!calculator.waitingForSecondNumber){
+        calculator.operator=operator;
+        calculator.waitingForSecondNumber=true;
+        calculator.firtNumber = calculator.displayNumber;
+    }else{
+        window.alert('operator sudah ada');
+    }
+}
+function performCalculation(){
+    if(calculator.firtNumber=='0' && calculator.operator==null){
+        window.alert('belum ada operator');
+        return;
+    }
+    if(calculator.operator==='+'){
+        calculator.displayNumber = (parseInt(calculator.firtNumber) + parseInt(calculator.displayNumber));
+    }else{
+        calculator.displayNumber = (parseInt(calculator.firtNumber) - parseInt(calculator.displayNumber));
+    }
+}
+//fungsi memasukkan digit
+function inputDigit(digit){
+    if(calculator.waitingForSecondNumber && (calculator.displayNumber===calculator.firtNumber)){
+        calculator.displayNumber= digit;
+    }
+    else{
+        if(calculator.displayNumber==='0'){
+            calculator.displayNumber=digit;
+        }else{
+            calculator.displayNumber+=digit;
+        }
+    }
+    console.log(calculator);
+}
+//mendapatkan button
+const buttons = document.querySelectorAll('.button');
+
+for(let button of buttons){
+    button.addEventListener('click',function(event){
+       
+        //mendapatkan objek elemen yang diklik
+        const target = event.target; //target disini adalah property dari object
+
+        if(target.classList.contains('clear')){
+            clearCalculator();
+            updateDisplay();
+            return;
+        }
+        if(target.classList.contains('negative')){
+            inverseNumber();
+            updateDisplay();
+            return;
+        }
+        if(target.classList.contains('equal')){
+            performCalculation();
+            updateDisplay();
+            return;
+        }
+        if(target.classList.contains('operator')){
+            handleOperator(target.innerText);
+            updateDisplay();
+            console.log(calculator);
+            return;
+        }
+        inputDigit(target.innerText);
+        updateDisplay();
+    })
+}
